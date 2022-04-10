3014af0e619aaffa1afa33ff7b78a5c217f70883
snandakumar87 <snandaku@redhat.com>
2022-04-10 16:52:57 -0400

    changed the kafka to point to managed service

--- src/main/resources/META-INF/resources/index.js
+++ src/main/resources/META-INF/resources/index.js
@@ -18,18 +18,17 @@ if (!!window.EventSource) {
          row+='</td><td>';}
 
          alert(row);
-//
-//         if(data.Transaction.amlAlert==='null' && data.Transaction.fraudAlert==='null'){
-//            row+='<div class="icon"><i class="ni ni-check-bold"></i></td>'
-//            row+='<td>No Investigation Required!';
-//         } else {
-//            row+='<div class="icon"><i class="ni ni-fat-remove"></i></td>';
-//            row+='<td><button class="btn btn-icon btn-primary" type="button" id="ajaxSubmit" onclick="checkout('+data.Transaction.transactionId+')"><span class="btn-inner--icon">Details<i class="ni ni-bold-right"></i></span></button>';
-//
-//         }
-//
-//
-//
+
+         if(data.Transaction.amlAlert==='null' && data.Transaction.fraudAlert==='null'){
+            row+='<div class="icon"><i class="ni ni-check-bold"></i></td>'
+
+         } else {
+            row+='<div class="icon"><i class="ni ni-fat-remove"></i></td>';
+
+         }
+
+
+
          row+='</tr><tr width="200px" id="svg" style="width:100%"></tr>';
 
         $('#tbody').append(row);
